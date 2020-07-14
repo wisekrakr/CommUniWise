dc742f78b6514a8d79c1ab67965465a673919e7b
Max <max.git@ufo.xs4all.nl>
2020-07-14 22:07:05 +0200

    getting somewhere

--- src/main/java/com/wisekrakr/communiwise/phone/audiovisualconnection/threads/TransmittingThread.java
+++ src/main/java/com/wisekrakr/communiwise/phone/audiovisualconnection/threads/TransmittingThread.java
@@ -1,81 +1,165 @@
 package com.wisekrakr.communiwise.phone.audiovisualconnection.threads;
 
-import com.wisekrakr.communiwise.phone.audiovisualconnection.processing.Encoder;
 import com.wisekrakr.communiwise.phone.audiovisualconnection.processing.PcmuEncoder;
-import com.wisekrakr.communiwise.phone.audiovisualconnection.rtp.Capture;
-import com.wisekrakr.communiwise.phone.audiovisualconnection.rtp.RTPSender;
+import com.wisekrakr.communiwise.phone.audiovisualconnection.rtp.RTPPacket;
+import com.wisekrakr.communiwise.phone.audiovisualconnection.rtp.RTPParser;
 
 import javax.sound.sampled.TargetDataLine;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.concurrent.CountDownLatch;
+import java.util.Arrays;
+import java.util.Random;
 
 public class TransmittingThread {
-
     private static final int PIPE_SIZE = 4096; // todo what size should it be? same as buffer size for the datagram packet?
+    private final DatagramSocket socket;
+    private final TargetDataLine targetDataLine;
 
-    private Capture capture;
-    private Encoder encoder;
-    private RTPSender rtpSender;
+    private Thread captureThread;
+    private Thread encoderThread;
+    private Thread rtpSenderThread;
 
-    public TransmittingThread(DatagramSocket socket, TargetDataLine targetDataLine) throws IOException {
-
-        CountDownLatch latch = new CountDownLatch(3);
-        PipedOutputStream rawDataOutput = new PipedOutputStream();
-        PipedInputStream rawDataInput;
-        try {
-            rawDataInput = new PipedInputStream(rawDataOutput, PIPE_SIZE);
-        } catch (IOException e) {
-            System.out.println("input/output error " + e.getMessage());
-            return ;
-        }
-
-        PipedOutputStream encodedDataOutput = new PipedOutputStream();
-        PipedInputStream encodedDataInput;
-        try {
-            encodedDataInput = new PipedInputStream(encodedDataOutput, PIPE_SIZE);
-        } catch (IOException e) {
-            System.out.println("input/output error " + e.getMessage());
-            rawDataInput.close();
-            return ;
-        }
-        capture = new Capture(rawDataOutput, latch, targetDataLine);
-
-        encoder = new PcmuEncoder(rawDataInput, encodedDataOutput,false, socket.getLocalAddress().getHostAddress(), latch);
-
-        rtpSender = new RTPSender(encodedDataInput, latch, socket);
-
+    public TransmittingThread(DatagramSocket socket, TargetDataLine targetDataLine) {
+        this.socket = socket;
+        this.targetDataLine = targetDataLine;
     }
 
-    public void start(){
-        capture.setStopped(false);
-        encoder.setStopped(false);
-        rtpSender.setStopped(false);
+    public void start() throws IOException {
+        PipedOutputStream rawDataOutput = new PipedOutputStream();
+        PipedInputStream rawDataInput = new PipedInputStream(rawDataOutput, PIPE_SIZE);
 
-        Thread captureThread = new Thread(capture,
-                Capture.class.getSimpleName());
-        Thread encoderThread = new Thread(encoder,
-                Encoder.class.getSimpleName());
-        Thread rtpSenderThread = new Thread(rtpSender,
-                RTPSender.class.getSimpleName());
+        PipedOutputStream encodedDataOutput = new PipedOutputStream();
+        PipedInputStream encodedDataInput = new PipedInputStream(encodedDataOutput, PIPE_SIZE);
+
+        captureThread = new Thread(() -> {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            try {
+                targetDataLine.start();
+
+                while (!Thread.currentThread().isInterrupted()) {
+                    int actuallyRead = targetDataLine.read(buffer, 0, buffer.length);
+
+                    rawDataOutput.write(buffer, 0, actuallyRead);
+
+                    if (actuallyRead < buffer.length) {
+                        break;
+                    }
+                }
+
+                targetDataLine.stop();
+
+                System.out.println("Capture thread has stopped");
+            } catch (Throwable e) {
+                System.out.println("Capture thread has stopped unexpectedly " + e.getMessage());
+            }
+        }, "Capture thread");
+
+
+        captureThread.setDaemon(true);
+        encoderThread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    byte[] rawBuffer = new byte[10000];
+                    byte[] encodingBuffer = new byte[10000];
+
+                    while (!Thread.currentThread().isInterrupted()) {
+                        int read = rawDataInput.read(rawBuffer);
+
+                        int encoded = PcmuEncoder.process(rawBuffer, encodingBuffer, 0, read);
+                        encodedDataOutput.write(encodingBuffer, 0, encoded);
+                    }
+
+                    System.out.println("Encoding thread has stopped");
+                } catch (Throwable e) {
+                    System.out.println("Encoding thread has stopped unexpectedly " + e.getMessage());
+                }
+            }
+        }, "Encoding thread");
+        encoderThread.setDaemon(true);
+        rtpSenderThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO: research meaning of all fields
+                RTPPacket rtpPacket = new RTPPacket();
+                rtpPacket.setVersion(2);
+                rtpPacket.setPadding(false);
+                rtpPacket.setExtension(false);
+                rtpPacket.setCsrcCount(0);
+                rtpPacket.setMarker(false);
+                rtpPacket.setPayloadType(0); //PCMU == 0   PCMA == 8    telephone-event == 101
+
+                Random random = new Random();
+                int sequenceNumber = random.nextInt();
+                rtpPacket.setSequenceNumber(sequenceNumber);
+                rtpPacket.setSsrc(random.nextInt());
+
+                byte[] buffer = new byte[1024];
+
+                int targetSize = 1;
+
+                int timestamp = 0;
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        int numBytesRead = 0;
+
+                        while (numBytesRead < targetSize) {
+                            numBytesRead += encodedDataInput.read(buffer, numBytesRead, buffer.length - numBytesRead);
+                        }
+
+                        rtpPacket.setData(Arrays.copyOf(buffer, numBytesRead));
+                        rtpPacket.setSequenceNumber(sequenceNumber++);
+
+                        timestamp += numBytesRead;
+                        rtpPacket.setTimestamp(timestamp);
+
+                        send(rtpPacket);
+                    }
+
+                    System.out.println("Sending thread has stopped");
+                } catch (Throwable e) {
+                    System.out.println("Sending thread has stopped unexpectedly " + e.getMessage());
+                }
+            }
+
+            private void send(RTPPacket rtpPacket) {
+                byte[] buf = RTPParser.encode(rtpPacket);
+                final DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length, socket.getInetAddress(), socket.getPort());
+
+                if (!socket.isClosed()) {
+                    try {
+                        socket.send(datagramPacket);
+
+                        System.out.println("Mic is sending data: " + datagramPacket.getLength()); //todo now:  datagramPacket.length == buf.length + 12 from encoded data
+
+                    } catch (IOException | SecurityException e) {
+                        System.out.println(" error while sending datagram packet " + e);
+                    }
+                }
+            }
+        }, "Send thread");
+        rtpSenderThread.setDaemon(true);
 
         captureThread.start();
         encoderThread.start();
         rtpSenderThread.start();
-
     }
 
     public void stop() {
-        if (capture != null) {
-            capture.setStopped(true);
-        }
-        if (encoder != null) {
-            encoder.setStopped(true);
-        }
-        if (rtpSender != null) {
-            rtpSender.setStopped(true);
-        }
+        captureThread.interrupt();
+        encoderThread.interrupt();
+        rtpSenderThread.interrupt();
     }
+
+    public static final int SAMPLE_SIZE = 16;
+    public static final int BUFFER_SIZE = SAMPLE_SIZE * 20;
+
+
+    /*
+
+
+     */
+
 }
